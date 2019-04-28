75688419fd86ad5c8bbd5667e11445c344a51d6b
Lei Hua <lhua@example.com>
2019-04-28 08:01:56 -0400

    static files

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,7 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +10,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
